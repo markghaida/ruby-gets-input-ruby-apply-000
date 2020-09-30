fc986c14e26c65e1571a4619c675e7cd41bc7742
Mark G <markghaida@gmail.com>
2020-09-30 23:50:09 +0000

    Automatically backed up by Learn

--- spec/lib/hello_ruby_programmer_spec.rb
+++ spec/lib/hello_ruby_programmer_spec.rb
@@ -1,6 +1,6 @@
 require 'hello_ruby_programmer'
 
-describe 'greeting (name)' do
+describe 'greeting' do
   it 'accepts an argument called `name`' do
     expect(STDOUT).to receive(:puts).with("Hi, you! Welcome to the wonderful world of Ruby programming.")
 
